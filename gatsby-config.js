7d3b8e3dac4cf5a60f1f2506cdec3064f0b4be31
James Dolan <dolanja@tcd.ie>
2021-11-26 17:47:56 +0000

    dev: basic blogging up and running

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,9 @@ module.exports = {
         path: `${__dirname}/blog`,
       }
     },
+    "gatsby-plugin-mdx",
+    "gatsby-remark-autolink-headers",
+    "gatsby-remark-prismjs",
+    "gatsby-remark-images"
   ],
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,28 +1,37 @@
 import * as React from 'react'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import { graphql } from 'gatsby'
+
 import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
-          data.allFile.nodes.map(node => (
-              <li key={node.name}>
-                  {node.name}
-              </li>
+          data.allMdx.nodes.map((node) => (
+              <article key={node.id}>
+                <h2>{node.frontmatter.title}</h2>
+                <p>Posted: {node.frontmatter.date}</p>
+                <MDXRenderer>
+                  {node.body}
+                </MDXRenderer>
+              </article>
           ))
       }
-      </ul>
     </Layout>
   )
 }
 
 export const query = graphql`
-  query {    
-    allFile {
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMM D, YYYY")
+          title
+        }
+        id
+        body
       }
     }
   }

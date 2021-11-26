f8d514b13458115f6ffe0978ac3eb3a55ea2f242
James Dolan <dolanja@tcd.ie>
2021-11-26 10:38:41 +0000

    dev: just after installing first plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
     title: "Portfolio",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };

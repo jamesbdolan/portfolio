e221cb7d3e2320f1293172ef2f5dd29fc892c495
James Dolan <dolanja@tcd.ie>
2021-11-26 18:54:20 +0000

    feat: file system route api & new blog to test

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,8 +1,7 @@
 import * as React from 'react'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
-import { graphql } from 'gatsby'
-
-import Layout from '../components/layout'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
   return (
@@ -10,11 +9,12 @@ const BlogPage = ({ data }) => {
       {
           data.allMdx.nodes.map((node) => (
               <article key={node.id}>
-                <h2>{node.frontmatter.title}</h2>
+                <h2>
+                  <Link to={`/blog/${node.slug}`}>
+                    {node.frontmatter.title}
+                  </Link>
+                </h2>
                 <p>Posted: {node.frontmatter.date}</p>
-                <MDXRenderer>
-                  {node.body}
-                </MDXRenderer>
               </article>
           ))
       }
@@ -32,6 +32,7 @@ export const query = graphql`
         }
         id
         body
+        slug
       }
     }
   }

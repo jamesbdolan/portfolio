4c2de8bb1502192db7a10a141a2d5e6eb19cba5b
James Dolan <dolanja@tcd.ie>
2021-11-30 17:34:50 +0000

    dev: add github link to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>Find the Github repository <a href="https://github.com/jamesbdolan/portfolio">here</a></p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="../images/clifford.png"

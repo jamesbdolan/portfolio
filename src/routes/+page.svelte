ac7911426ec4c887527668c691c2afa7c1f9d614
James Dolan <dolanja@tcd.ie>
2023-04-20 15:36:58 +0100

    feat: tried to add firebase but paused for now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
+<!-- <script>
+	import { getAnalytics, logEvent } from 'firebase/analytics';
+	import { onMount } from 'svelte';
+	import { app } from '../firebase';
+	onMount(() => {
+		const analytics = getAnalytics(app);
+		logEvent(analytics, 'page_title');
+	});
+</script> -->
+
 <h1>Hello! I'm James, a software engineer in Dublin</h1>
 <p>
-    This site is a WIP, I am following the Sveltekit tutorial.
+    This site is a WIP, I am following Svelte tutorials.
     I am currently at <a href="https://kit.svelte.dev/docs/load">this stage.</a>
 </p>

81369e36c88a3b1e5dbe861cb14e51e7e87b6fbd
James Dolan <dolanja@tcd.ie>
2023-04-20 15:52:45 +0100

    feat: firebase script should be in layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,3 @@
-<!-- <script>
-	import { getAnalytics, logEvent } from 'firebase/analytics';
-	import { onMount } from 'svelte';
-	import { app } from '../firebase';
-	onMount(() => {
-		const analytics = getAnalytics(app);
-		logEvent(analytics, 'page_title');
-	});
-</script> -->
-
 <h1>Hello! I'm James, a software engineer in Dublin</h1>
 <p>
     This site is a WIP, I am following Svelte tutorials.

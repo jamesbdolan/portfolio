1f621b3d0c9138999d2c178b382b294fc84561b2
James Dolan <dolanja@tcd.ie>
2023-10-17 16:28:33 +1100

    fix and move get in touch button

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -28,7 +28,7 @@
 		{/each}
 	</div>
 
-	<button
+	<a
 		href="https://linkedin.com/in/jamesbdolan"
 		target="_blank"
 		class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
@@ -36,6 +36,6 @@
 		<div
 			class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
 		/>
-		<h4 class="relative z-9">Get in touch</h4>
-	</button>
+		<h4 class="relative z-9 py-2">Get in touch</h4>
+	</a>
 </header>

27a9c96d87eafbccec03bf87f1a12aeda095f47c
James Dolan <dolanja@tcd.ie>
2024-09-03 23:25:05 +1000

    feat: calm down (#11)

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -9,7 +9,7 @@
 
 <header
 	class={'sticky z-[10] top-0 duration-200 px-6 flex items-counter justify-between border border-solid ' +
-		(y > 0 ? ' py-4 bg-slate-950 border-violet-950' : ' py-6 bg-transparent border-transparent')}
+		(y > 0 ? ' py-4 bg-slate-950 border-green-950' : ' py-6 bg-transparent border-transparent')}
 >
 	<h1 class="font-medium">
 		<a href="/">
@@ -20,7 +20,7 @@
 		{#each tabs as tab, index}
 			<a
 				href={tab.link}
-				class="duration-200 hovver text-violet-400"
+				class="duration-200 hovver text-green-500"
 				target={index === 2 ? '_blank' : ''}
 			>
 				<p>{tab.name}</p>
@@ -31,10 +31,10 @@
 	<a
 		href="https://linkedin.com/in/jamesbdolan"
 		target="_blank"
-		class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+		class="greenShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
 	>
 		<div
-			class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+			class="absolute top-0 right-full w-full h-full bg-green-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
 		/>
 		<h4 class="relative z-9 py-2">Get in touch</h4>
 	</a>
